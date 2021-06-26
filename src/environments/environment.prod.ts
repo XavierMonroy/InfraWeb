d4bd4b004a7cdf0a1c853318f599ca28c7bd8ceb
XavierMonroy <luxamonroygu@ittepic.edu.mx>
2021-06-26 02:10:37 -0600

    Alertas emergentes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBITYmO1WQWmYz2COwGoeporXS-kxnMyYs",
-    authDomain: "ittgym.firebaseapp.com",
-    projectId: "ittgym",
-    storageBucket: "ittgym.appspot.com",
-    messagingSenderId: "1083967016287",
-    appId: "1:1083967016287:web:83462b2ff089e48229baea",
-    measurementId: "G-7LH5E10WW6"
+    apiKey: "AIzaSyCMbR5rlOx1L46JPBHVMiOrjXdfuyndqZo",
+    authDomain: "ittgym-489fc.firebaseapp.com",
+    projectId: "ittgym-489fc",
+    storageBucket: "ittgym-489fc.appspot.com",
+    messagingSenderId: "321650493321",
+    appId: "1:321650493321:web:070c75a740d86900998358",
+    measurementId: "G-5E4V0VC4QV"
   }
 };
